b2b509564f6a0e91b080650f6fc4d705cfa18b6e
unknown6656 <unknown84@gmx.de>
2021-06-20 20:08:23 +0200

    added assembly info

---
+++ NativeInterop/assembly_info.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+using namespace System::Reflection;
+using namespace System;
+
+
+[assembly:AssemblyTitleAttribute(L"NativeInterop")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"NativeInterop")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) unknown6656, 2019-2021")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:ComVisible(false)];
