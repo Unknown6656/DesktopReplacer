97fb4bd9f124747b986411ace39394e4a38b68df
unknown6656 <unknown84@gmx.de>
2021-06-20 15:41:38 +0200

    added replacer window

---
+++ DesktopReplacer/DesktopReplacerWindow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System;
+
+using CefSharp.WinForms;
+
+namespace DesktopReplacer
+{
+    public sealed class DesktopReplacerWindow
+        : Form
+    {
+        public DesktopWindow DesktopWindow { get; }
+        public ChromiumWebBrowser Browser { get; }
+
+
+        public DesktopReplacerWindow()
+        {
+            DesktopWindow = Desktop.GetDesktopWindow();
+            Browser = new("https://unknown6656.com/");
+            Controls.Add(Browser);
+
+            Load += DesktopReplacerWindow_Load;
+            FormClosing += DesktopReplacerWindow_FormClosing;
+        }
+
+        ~DesktopReplacerWindow() => DesktopWindow.Restore();
+
+        private void DesktopReplacerWindow_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void DesktopReplacerWindow_Load(object? sender, EventArgs e)
+        {
+            MonitorInfo[] monitors = Desktop.FetchMonitors();
+
+
+
+
+
+
+
+            DesktopWindow.Hijack(this);
+        }
+    }
+}
