43395800d0a7bf1f42999887c550f1b2cb8a4c68
unknown6656 <unknown84@gmx.de>
2021-06-26 18:58:38 +0200

    added the code generator

--- DesktopReplacer/DesktopReplacerWindow.cs
+++ DesktopReplacer/DesktopReplacerWindow.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Threading.Tasks;
 using System.ComponentModel;
-using System.Data;
+using System.Windows.Forms;
 using System.Diagnostics;
+using System.Reflection;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System;
 
 using Microsoft.Web.WebView2.WinForms;
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Win32;
 
 using Unknown6656.IO;
 
@@ -21,12 +18,16 @@ namespace DesktopReplacer
     public sealed partial class DesktopReplacerWindow
         : Form
     {
-        private static readonly DirectoryInfo DIR = AppDomain.CurrentDomain.SetupInformation.ApplicationBase is string dir ? new DirectoryInfo(dir) : new FileInfo(Assembly.GetExecutingAssembly().Location).Directory!;
-        private static readonly DirectoryInfo USER_DATA_DIR = DIR.CreateSubdirectory("webview2-cache");
+        internal const string EXPLORER = "explorer";
+        internal const string EDGE_ARGS = "--disable-web-security --allow-file-access-from-files --allow-file-access";
+        internal static readonly DirectoryInfo DIR = AppDomain.CurrentDomain.SetupInformation.ApplicationBase is string dir ? new DirectoryInfo(dir) : new FileInfo(Assembly.GetExecutingAssembly().Location).Directory!;
+        internal static readonly DirectoryInfo RESOURCE_DIR = DIR.CreateSubdirectory("resources");
+        internal static readonly DirectoryInfo USER_DATA_DIR = DIR.CreateSubdirectory("webview2-cache");
 
 
         public DesktopWindow DesktopWindow { get; }
         public MonitorInfo[] Monitors { get; private set; }
+        public HTMLGenerator Generator { get; }
         public WebView2 Browser { get; }
 
 
@@ -34,6 +35,7 @@ namespace DesktopReplacer
         {
             DesktopWindow = Desktop.GetDesktopWindow();
             Monitors = Array.Empty<MonitorInfo>();
+            Generator = new();
             Browser = new();
             (Browser as ISupportInitialize)?.BeginInit();
 
@@ -51,8 +53,20 @@ namespace DesktopReplacer
                 Text = "Close",
             };
             button.Click += (_, _) => Close();
-
             Controls.Add(button);
+            button = new()
+            {
+                Location = new(200, 240),
+                Size = new(100, 30),
+                Text = "Redraw",
+            };
+            button.Click += (_, _) =>
+            {
+                Generator.ResetTemplateCache();
+                UpdateDekstop();
+            };
+            Controls.Add(button);
+
             Controls.Add(Browser);
 
             AllowTransparency = true;
@@ -65,18 +79,43 @@ namespace DesktopReplacer
             ResumeLayout(false);
 
             Load += DesktopReplacerWindow_Load;
+            FormClosing += DesktopReplacerWindow_FormClosing;
         }
 
         ~DesktopReplacerWindow() => DesktopWindow.Restore();
 
+        private void SystemEvents_DisplaySettingsChanged(object? sender, EventArgs e) => UpdateDekstop();
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category is UserPreferenceCategory.Desktop
+                           or UserPreferenceCategory.General
+                           or UserPreferenceCategory.Icon
+                           or UserPreferenceCategory.Window
+                           or UserPreferenceCategory.VisualStyle)
+                UpdateDekstop();
+        }
+
+        private void DesktopReplacerWindow_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            SystemEvents.DisplaySettingsChanged -= SystemEvents_DisplaySettingsChanged;
+            SystemEvents.UserPreferenceChanged -= SystemEvents_UserPreferenceChanged;
+
+            DesktopWindow.Restore();
+        }
+
         private async void DesktopReplacerWindow_Load(object? sender, EventArgs e)
         {
             CoreWebView2Environment environment = await FetchWW2Environment();
-            CoreWebView2Controller controller = await environment.CreateCoreWebView2ControllerAsync(Handle);
 
-            UpdateDekstop();
+            await Browser.EnsureCoreWebView2Async(environment);
 
             DesktopWindow.Hijack(this);
+
+            SystemEvents.DisplaySettingsChanged += SystemEvents_DisplaySettingsChanged;
+            SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+
+            await Task.Delay(300).ContinueWith(_ => UpdateDekstop());
         }
 
         private static async Task<CoreWebView2Environment> FetchWW2Environment()
@@ -87,7 +126,7 @@ retry:
             {
                 environment ??= await CoreWebView2Environment.CreateAsync(null, USER_DATA_DIR.FullName, new CoreWebView2EnvironmentOptions()
                 {
-                    // TODO
+                    AdditionalBrowserArguments = EDGE_ARGS,
                 });
             }
             catch (Exception)
@@ -118,7 +157,7 @@ The WebView2 runtime could not be found. How would like to proceed?
             return environment;
         }
 
-        private void UpdateDekstop()
+        private void UpdateDekstop() => Invoke(new MethodInvoker(delegate
         {
             Monitors = Desktop.FetchMonitors();
 
@@ -140,30 +179,58 @@ The WebView2 runtime could not be found. How would like to proceed?
             Width = m_right - m_left;
             Height = m_bottom - m_top;
 
-            string html = GenerateHTMLCode();
+            (RawIconInfo[], double, ViewMode)? icons = Desktop.FetchDesktopIcons();
+            GenerationResult generated = Generator.GenerateDesktopHTMLCode(Bounds, Monitors, Desktop.FetchDesktopImagePath(), icons);
 
             Browser.Left = 0;
             Browser.Top = 0;
             Browser.Width = Width;
             Browser.Height = Height;
             Browser.SendToBack();
-            Browser.NavigateToString(html);
-        }
 
-        private string GenerateHTMLCode()
+            try
+            {
+                Browser.Source = new Uri("file://" + generated.OutPath);
+                Browser.Reload();
+                // Browser.NavigateToString(generated.HTML);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }));
+
+        public static async Task<bool> RestartExplorer()
         {
+            try
+            {
+                Process[]? procs = Process.GetProcessesByName(EXPLORER);
 
-            return @"
-<!DOCTYPE html>
-<html lang=""en"">
-<body style=""font-size:7em;"">
-<h1>top kek</h1>
-lol
-</body>
-</html>
-";
+                if (procs.Length is 0)
+                {
+                    using Process? p = new()
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = EXPLORER,
+                            UseShellExecute = false,
+                            RedirectStandardError = false,
+                            RedirectStandardOutput = false,
+                        }
+                    };
 
+                    p.Start();
 
+                    await Task.Delay(300); // TODO : fix this dogshite
+
+                    return true;
+                }
+            }
+            catch
+            {
+            }
+
+            return false;
         }
     }
 }
---
+++ DesktopReplacer/HTMLGenerator.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+using Microsoft.WindowsAPICodePack.Shell;
+
+using Unknown6656.Common;
+using Unknown6656.IO;
+
+namespace DesktopReplacer
+{
+    public sealed record GenerationResult(string HTML, string OutPath);
+
+    public sealed class HTMLGenerator
+    {
+        private const string TEMP_PREFIX = "--temp-processed--";
+        private static readonly Regex REPLACEMENT_REGEX = new(@"§(?<varname>\w+)§", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private readonly Dictionary<string, string> _source_cache = new();
+        private readonly object _mutex = new();
+
+
+        public string ReadTemplate(string file)
+        {
+            if (_source_cache.TryGetValue(file, out string? source))
+                return source;
+
+            string path = Path.Combine(DesktopReplacerWindow.RESOURCE_DIR.FullName, file);
+
+            return _source_cache[file] = DataStream.FromFile(path).ToString(Encoding.Default).TrimStart('\xfeff');
+        }
+
+        public GenerationResult ModifyTemplate(string file, Dictionary<string, object?> replacements, bool write_back = false)
+        {
+            string source = ReadTemplate(file);
+            string replaced = REPLACEMENT_REGEX.Replace(source, match => replacements.TryGetValue(match.Groups["varname"].Value, out object? value) ? value?.ToString() ?? "" : match.Value);
+            string out_path = Path.Combine(DesktopReplacerWindow.USER_DATA_DIR.FullName, TEMP_PREFIX + file);
+
+            if (write_back)
+                DataStream.FromString(replaced, Encoding.Default).ToFile(out_path);
+
+            return new(replaced, out_path.Replace('\\', '/'));
+        }
+
+        public void ResetTemplateCache() => _source_cache.Clear();
+
+
+        public GenerationResult GenerateDesktopHTMLCode(Rectangle window_bounds, MonitorInfo[] monitors, string img_path, (RawIconInfo[], double, ViewMode)? icons) =>
+            ModifyTemplate("desktop-template.html", new(StringComparer.InvariantCultureIgnoreCase)
+            {
+                ["style"] = GenerateDesktopCSSStyle(img_path).HTML,
+                ["width"] = window_bounds.Width,
+                ["height"] = window_bounds.Height,
+                ["monitors"] = monitors.Select(m => GenerateMonitorHTMLCode(m, window_bounds.Top, window_bounds.Left, icons).HTML).StringJoinLines(),
+                ["script"] = GenerateJSCode().HTML,
+            }, write_back: true);
+
+        public GenerationResult GenerateJSCode() =>
+            ModifyTemplate("script-template.js", new(StringComparer.InvariantCultureIgnoreCase)
+            {
+                // TODO
+            });
+
+        public GenerationResult GenerateDesktopCSSStyle(string img_path) =>
+            ModifyTemplate("style-template.css", new(StringComparer.InvariantCultureIgnoreCase)
+            {
+                ["background"] = img_path.Replace('\\', '/'),
+                // TODO
+            });
+
+        public GenerationResult GenerateMonitorHTMLCode(MonitorInfo monitor, int window_top, int window_left, (RawIconInfo[], double, ViewMode)? icons)
+        {
+            StringBuilder sb_icons = new();
+
+            if (monitor.IsPrimary /* TODO : fix this */ && icons is (RawIconInfo[] rawicons, double icon_size, ViewMode view_mode))
+                _ = Parallel.ForEach(rawicons, icon =>
+                {
+                    double width = icon_size + 60;
+                    double height = icon_size + 50;
+                    string? icon_source = null;
+                    bool hidden = false;
+
+                    if (icon.MatchingFiles.Length > 0 && icon.MatchingFiles[0] is { } file)
+                        try
+                        {
+                            hidden = file.Attributes.HasFlag(FileAttributes.Hidden);
+
+                            using ShellFile shell_file = ShellFile.FromFilePath(file.FullName);
+
+                            icon_source = DataStream.FromBitmap(shell_file.Thumbnail.Bitmap, ImageFormat.Png).ToDataURI("image/png");
+                        }
+                        catch
+                        {
+                        }
+
+                    string html = ModifyTemplate("icon-template.html", new(StringComparer.InvariantCultureIgnoreCase)
+                    {
+                        ["name"] = icon.DisplayName,
+                        ["files"] = $"[{icon.MatchingFiles.Select(f => $"'{f.FullName}'").StringJoin(",")}]",
+                        ["width"] = width,
+                        ["height"] = height,
+                        ["left"] = icon.X * width,
+                        ["top"] = icon.Y * height,
+                        ["image"] = icon_source,
+                        ["icon_size"] = icon_size,
+
+                        // TODO
+                    }).HTML;
+
+                    lock (_mutex)
+                        _ = sb_icons.AppendLine(html);
+                });
+
+            return ModifyTemplate("monitor-template.html", new(StringComparer.InvariantCultureIgnoreCase)
+            {
+                ["position_top"] = monitor.Top - window_top,
+                ["position_left"] = monitor.Left - window_left,
+                ["top"] = monitor.Top,
+                ["left"] = monitor.Left,
+                ["right"] = monitor.Right,
+                ["bottom"] = monitor.Bottom,
+                ["width"] = monitor.Width,
+                ["height"] = monitor.Height,
+                ["name"] = monitor.Name,
+                ["primary"] = monitor.IsPrimary ? "(primary)" : "",
+                ["frequency"] = monitor.Frequency,
+                ["icons"] = sb_icons,
+            });
+        }
+    }
+}
